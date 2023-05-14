7f2055502c03d8f8ef4de8744a2796c72db1419d
jithunmadhav <jithunmadhav1@gmail.com>
2023-05-14 19:03:33 +0530

    mechanic signup ui updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import UserLogin from './components/UserLogin/UserLogin';
 import AdminLogin from './components/AdminLogin/AdminLogin';
 import MechanicLogin from './components/MechanicLogin/MechanicLogin';
 import UserSignup from './components/UserSignup/UserSignup';
+import MechanicSignup from './components/MechanicSignup/MechanicSignup';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route  element={<UserSignup/>} path='/userSignup'/>
         <Route  element={<AdminLogin/>} path='/adminLogin' />
         <Route  element={<MechanicLogin/> } path='/mechanicLogin' />
+        <Route  element={<MechanicSignup/> } path='/mechanicSignup' />
+
       </Routes>
      </Router>
     </div>
---
+++ client/src/components/MechanicSignup/MechanicSignup.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './MechanicSignup.css'
+function MechanicSignup() {
+  return (
+    <div>
+      <div className='signup-background'>
+       <div className='Signup'>
+  <div className='Signup-connect-mechanic '>
+  </div>
+  <div className='Signup-classic'>
+    
+    <form className='Form'>
+      <fieldset className='username'>
+        <input type="text" placeholder="username" required/>
+      </fieldset>
+      <fieldset className='email'>
+        <input type="email" placeholder="email" required/>
+      </fieldset>
+      <fieldset className='password'>
+        <input type="text" placeholder="Mobile"  required/>
+      </fieldset>
+      <fieldset className='password'>
+        <input type="password" placeholder="password"  required/>
+      </fieldset>
+      <fieldset className='password'>
+        <input type="password" placeholder="confirm password"  required/>
+      </fieldset>
+      <button type="submit" className="btn">sign up</button>
+    </form>
+  </div>
+</div>
+    </div>
+    </div>
+  )
+}
+
+export default MechanicSignup
