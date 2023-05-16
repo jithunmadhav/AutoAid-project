de01b1e51433713f02ae15953df6c8d810335920
jithunmadhav <jithunmadhav1@gmail.com>
2023-05-16 23:32:22 +0530

    service controller updated

--- server/controller/serviceController.js
+++ server/controller/serviceController.js
@@ -1,5 +1,12 @@
+import mechanicModel from "../model/mechanicModel"
 
 
 export const twoWheelerBooking=async(req,res)=>{
-    
+    let {location}=req.body
+    let result= await mechanicModel.find({location:location})
+    if(result){
+        res.json({err:false,result})
+    }else{
+        res.json({err:true})
+    }
 }
