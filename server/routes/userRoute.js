9563a1c18f4b71e7dbced6c2f2b2e6cab9e2f916
jithunmadhav <jithunmadhav1@gmail.com>
2023-05-29 16:30:17 +0530

    forgot password updated

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,11 @@
 import  express  from 'express';
-import { resendOtp, userLogin, userLogout, userSignup, verifyUserSignup } from '../controller/userController.js';
+import { forgotPassword, resendOtp, resetpassword, userLogin, userLogout, userSignup, verifyUserSignup } from '../controller/userController.js';
 import { userCheckAuth } from '../middleware/userAuth.js';
 const router=express.Router()
 
 router.get('/auth',userCheckAuth)
 router.post('/signup',userSignup).post('/verifySignup',verifyUserSignup).post('/resendOtp',resendOtp)
-
+router.post('/forgotPassword',forgotPassword).post('/resetPassword',resetpassword)
 router.post('/login',userLogin).get('/logout',userLogout)
 
 
