0e7a7f947a8c54509a45a1c3f653016fab8679cb
jithunmadhav <jithunmadhav1@gmail.com>
2023-05-31 22:41:45 +0530

    mechanic login updated

--- client/src/components/MechanicLogin/MechanicLogin.jsx
+++ client/src/components/MechanicLogin/MechanicLogin.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MechanicLogin.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from '../../axios'
+import { useDispatch } from 'react-redux'
+
 function MechanicLogin() {
+  const dispatch = useDispatch()
+  const navigate=useNavigate()
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+  const [err, seterr] = useState('')
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    if(email.trim() && password.trim()){
+      axios.post('/mechanic/login',{email,password}).then((response)=>{
+        if(!response.data.err){
+          dispatch({type:'refresh'})
+          navigate('/mechanic/dashboard')
+        }
+      })
+    }else{
+      seterr('All fields are required')
+    }
+  }
   return (
     <>
         <div className="gray-background ">
@@ -10,12 +31,13 @@ function MechanicLogin() {
    
   </div>
   <div className='signup-classic'>
-    <form  className='form'>
+  <p className='errorMessage'>{err}</p>
+    <form onSubmit={handleSubmit} className='form'>
       <fieldset className='email'>
-        <input type="email" placeholder="email"   required/>
+        <input type="email" value={email} onChange={(e)=>setemail(e.target.value)} placeholder="email"   required/>
       </fieldset>
       <fieldset className='password'>
-        <input type="password" placeholder="password"   required/>
+        <input type="password" placeholder="password"  value={password} onChange={(e)=>setpassword(e.target.value)}  required/>
       </fieldset>
       <button type="submit" style={{ color:'white' }}  className="btn">Login</button>
     </form>
