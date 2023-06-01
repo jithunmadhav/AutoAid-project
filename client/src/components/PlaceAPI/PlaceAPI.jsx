d8d373ab95388ddbb609a00253673d1d84aaf306
jithunmadhav <jithunmadhav1@gmail.com>
2023-06-01 22:28:13 +0530

    admin side updated

--- client/src/components/PlaceAPI/PlaceAPI.jsx
+++ client/src/components/PlaceAPI/PlaceAPI.jsx
@@ -1,53 +1,63 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Slide from '@mui/material/Slide';
 
-import React, { useState } from 'react';
-import axios from 'axios';
+export default function ButtonAppBar() {
+  const [showLinks, setShowLinks] = React.useState(false);
 
-const PlaceAPI = () => {
-  const [searchValue, setSearchValue] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
-
-  const handleSearchChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
-    fetchSuggestions(value);
-  };
-
-  const fetchSuggestions = async (value) => {
-    try {
-      const response = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(value)}.json?access_token=pk.eyJ1Ijoiaml0aHVuIiwiYSI6ImNsaWEzZjg1NzBuMngzZHBnOWZzeTJ3eDMifQ.QUWNrEcjjYw_-HbBUDquhw`
-      );
-      const suggestions = response.data.features.map((feature) => feature.place_name);
-      setSuggestions(suggestions);
-    } catch (error) {
-      console.error('Error fetching suggestions:', error);
-    }
-  };
-
-  const handleSuggestionClick = (suggestion) => {
-    setSearchValue(suggestion);
-    setSuggestions([]);
+  const handleMenuClick = () => {
+    setShowLinks(!showLinks);
   };
 
   return (
-    <div>
-      <div>
-        <input
-          type="text"
-          placeholder="Search"
-          value={searchValue}
-          onChange={handleSearchChange}
-        />
-      </div>
-      <div>
-        {suggestions.map((suggestion) => (
-          <div key={suggestion} onClick={() => handleSuggestionClick(suggestion)}>
-            {suggestion.substring(0, 20)}
-          </div>
-        ))}
-      </div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: 'black' }}>
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            News
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Button color="inherit">Login</Button>
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuClick}
+            sx={{
+              transition: 'transform 0.3s', // Add CSS transition for animation
+              transform: showLinks ? 'rotate(45deg)' : 'none', // Rotate the icon when showLinks is true
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+        <Slide
+          direction="down"
+          in={showLinks}
+          mountOnEnter
+          unmountOnExit
+          timeout={{ enter: 300, exit: 0 }} // Adjust animation duration as needed
+        >
+          <Box sx={{ padding: '10px', display: showLinks ? 'block' : 'none' }}>
+            <Button color="inherit" sx={{ display: 'block', my: 1 }}>
+              Link 1
+            </Button>
+            <Button color="inherit" sx={{ display: 'block', my: 1 }}>
+              Link 2
+            </Button>
+            <Button color="inherit" sx={{ display: 'block', my: 1 }}>
+              Link 3
+            </Button>
+          </Box>
+        </Slide>
+      </AppBar>
+    </Box>
   );
-};
-
-export default PlaceAPI;
+}
