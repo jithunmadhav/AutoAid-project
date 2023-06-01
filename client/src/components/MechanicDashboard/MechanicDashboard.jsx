a8c32c7516bb0129ba37ca14b45e4cd34cf0d43c
jithunmadhav <jithunmadhav1@gmail.com>
2023-06-01 14:16:55 +0530

    Mechanic dashboard updated

--- client/src/components/MechanicDashboard/MechanicDashboard.jsx
+++ client/src/components/MechanicDashboard/MechanicDashboard.jsx
@@ -5,32 +5,72 @@ import './MechanicDashboard.css'
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 function MechanicDashboard() {
   const cardData = [
     {
-      title: 'Breakdown support',
+      title: 'Total Revenue',
       value:0
     },
     {
-      title: '2 wheeler support',
+      title: 'Pending Appointments',
       value:0
     },
     {
-      title: '4,6 wheeler support',
+      title: 'Completed Appointments',
       value:0
     },
+    
    
   ];
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+
   return (
    <>
   <div className='dashboard-background'>
     <div className='first-div'>
-    <div className='cards'>
+    <div className='cards-mechanics'>
         {cardData.map((card, index) => (
-          <Card key={index} sx={{ maxWidth: 345 }} style={{ borderRadius:'15px' }}>
+          <Card key={index} sx={{ maxWidth: 345 }} style={{ borderRadius:'15px' ,width:'280px'}}>
             <CardActionArea>
-            <Typography gutterBottom variant="h5" component="div" style={{ fontFamily:'Monomaniac One, sans-serif',textAlign:'center'}}>
+            <Typography gutterBottom variant="h5" component="div" style={{ fontFamily:'Monomaniac One, sans-serif',textAlign:'center',fontSize:'35px'}}>
                   {card.title}
                   
                 </Typography>
@@ -43,6 +83,34 @@ function MechanicDashboard() {
             </CardActionArea>
           </Card>
         ))}
+        </div>
+        <div className='table-div'>
+        <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+            <StyledTableCell align="right">Calories</StyledTableCell>
+            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.fat}</StyledTableCell>
+              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
         </div>
     </div>
   </div>
---
+++ client/src/components/MechanicProfile/MechanicProfile.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import './Profile.css'
+import axios from '../../axios'
+import { useNavigate } from 'react-router-dom'
+
+function MechanicProfile() {
+    const dispatch=useDispatch()
+    const navigate=useNavigate()
+    const {mechanic} =useSelector((state)=>{
+      return state
+    })
+    const handleLogout=()=>{
+      axios.get('/mechanic/logout').then((response)=>{
+        if(!response.data.err){
+          console.log(response.data);
+          dispatch({type:'refresh'})
+          return navigate('/mechanic/login')
+        }
+      })
+    }
+  return (
+    <div className='profile-background'>
+      <div className='inner-div'>
+        <h3 className='heading'>Profile</h3>
+        <button className='logout-btn' onClick={handleLogout} >Logout</button>
+        <div className='inner-div2'>
+          <div className='div2-sub1'>
+            <div className='profile-img'>
+            <img src= 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1680295524~exp=1680296124~hmac=02e23136e23578ef52071ce6ce8be6ecd2a32c6bef946fcacd4e6e788ed33360' style={{objectFit:'cover'}} alt='' width="100"  className="rounded-circle" />
+            </div>
+            <div>
+            <p  className='name'>{mechanic.details[0].name}</p>
+            <p style={{ marginTop:'-17px' }} className='name'>{mechanic.details[0].email}</p>
+            <button className='edit-btn'>Edit</button>
+
+            </div>
+          </div>
+          <div className='div2-sub2'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MechanicProfile
