7d577fa0f231518fcd7ee943e2f935f6a370f925
jithunmadhav <jithunmadhav1@gmail.com>
2023-06-06 18:11:07 +0530

    map updated

--- client/src/components/PlaceAPI/PlaceAPI.jsx
+++ client/src/components/PlaceAPI/PlaceAPI.jsx
@@ -1,21 +1,40 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-function PlaceAPI() {
-  const [currLocation, setcurrLoca] = useState('')
+mapboxgl.accessToken = 'pk.eyJ1Ijoiaml0aHVuIiwiYSI6ImNsaWEzZjg1NzBuMngzZHBnOWZzeTJ3eDMifQ.QUWNrEcjjYw_-HbBUDquhw';
+
+export default function MapWithGeolocation() {
+  const mapContainer = useRef(null);
+const [latitude, setlatitude] = useState('')
+const [longitude, setlongitude] = useState('')
   useEffect(() => {
- getLoc()
-  }, [])
-  const getLoc=()=>{
-    axios.get('https://ipapi.co/json').then((response)=>{
-      console.log(response);
-    })
-  }
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    navigator.geolocation.getCurrentPosition((position)=>{
+setlatitude(position.coords.latitude)
+setlongitude(position.coords.longitude)
 
-export default PlaceAPI
+    })
+    // Initialize the map
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [longitude,latitude], // Default center coordinates
+      zoom: 13, // Default zoom level
+    });
+
+    // Add geolocate control to the map
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      })
+      );
+      
+        var marker = new mapboxgl.Marker()
+        .setLngLat([longitude, latitude]) // Example: New York City
+        .addTo(map);
+  }, [latitude,longitude]);
+
+  return <div ref={mapContainer} style={{ height: '100vh' }} />;
+}
