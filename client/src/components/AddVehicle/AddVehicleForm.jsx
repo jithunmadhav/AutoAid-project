7d272dad8846484cc9849a121ee7315ab2727bb5
jithunmadhav <jithunmadhav1@gmail.com>
2023-06-08 21:20:16 +0530

    Add vehicle updated

--- client/src/components/AddVehicle/AddVehicleForm.jsx
+++ client/src/components/AddVehicle/AddVehicleForm.jsx
@@ -16,6 +16,35 @@ function AddVehicleForm() {
       <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
 </svg>
              </button>
+             <div>
+             <div className='vehicle-user'>
+  <div className='vehicle-connect'>
+  </div>
+  <div className='vehicle-classic'>
+  {/* <p className='errorMessage'>{err}</p> */}
+    <form className='Form' >
+      <fieldset className='username'>
+        <input type="text" placeholder="username"  required/>
+      </fieldset>
+      <fieldset className='email'>
+        <input type="email" placeholder="email" required/>
+      </fieldset>
+      <fieldset className='password'>
+        <input type="text" placeholder="Mobile"   required/>
+      </fieldset>
+      <fieldset className='password'>
+        <input type="password" placeholder="password"  required/>
+      </fieldset>
+      <fieldset className='password'>
+        <input type="password" placeholder="confirm password"    required/>
+      </fieldset>
+      <button type="submit" style={{ color:'white' }}  className="btn">sign up</button>
+      
+    </form>
+
+  </div>
+</div>
+             </div>
       </div>
     </div>
   )
