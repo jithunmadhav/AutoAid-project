c80db67e8937c8ce46a17f281a318aef27c68059
jithunmadhav <jithunmadhav1@gmail.com>
2023-06-08 21:35:21 +0530

    Add vehicle updated

--- client/src/components/AddVehicle/AddVehicleForm.jsx
+++ client/src/components/AddVehicle/AddVehicleForm.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
 import AddVehiclePage from '../../Pages/AddVehiclePage'
-
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
 function AddVehicleForm() {
+   
+    const [fuel, setfuel] = useState([])
     const [closeform, setcloseform] = useState(false)
     const closeForm=()=>{
         setcloseform(true)
@@ -22,23 +27,39 @@ function AddVehicleForm() {
   </div>
   <div className='vehicle-classic'>
   {/* <p className='errorMessage'>{err}</p> */}
-    <form className='Form' >
+    <form style={{ marginTop:'-43px' }} className='Form' >
       <fieldset className='username'>
-        <input type="text" placeholder="username"  required/>
+        <input type="text" placeholder="Manufacture"  required/>
       </fieldset>
       <fieldset className='email'>
-        <input type="email" placeholder="email" required/>
+        <input type="email" placeholder="Vehicle name" required/>
       </fieldset>
       <fieldset className='password'>
-        <input type="text" placeholder="Mobile"   required/>
+        <input type="text" placeholder="Reg no"   required/>
       </fieldset>
       <fieldset className='password'>
-        <input type="password" placeholder="password"  required/>
+        <input type="number" placeholder="Kilometer"  required/>
       </fieldset>
+         <FormControl sx={{ m: 1, minWidth: 250 ,}} size='small'> 
+           <InputLabel style={{ fontSize:'14px', marginTop:'5px' }}  id="demo-simple-select-helper-label">Fuel type</InputLabel>
+          <Select
+           labelId="demo-simple-select-helper-label"
+           id="demo-simple-select-helper"
+           multiple
+             value={fuel}
+            onChange={(event) => setfuel(event.target.value)}
+          className="select-input" // Add a custom CSS class
+           >
+        <MenuItem value="">
+        </MenuItem>
+        <MenuItem value={'Petrol'}>Petrol</MenuItem>
+        <MenuItem value={'Diesel'}>Diesel</MenuItem>
+        <MenuItem value={'Eletric'}>Eletric</MenuItem> </Select>
+        </FormControl>
       <fieldset className='password'>
-        <input type="password" placeholder="confirm password"    required/>
+        <input type="text" placeholder="Manufacture year"    required/>
       </fieldset>
-      <button type="submit" style={{ color:'white' }}  className="btn">sign up</button>
+      <button type="submit" style={{ color:'white' }}  className="btn">submit</button>
       
     </form>
 
