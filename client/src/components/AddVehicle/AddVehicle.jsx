984296fff96c73d54dc4b078744746d0d0c5c049
jithunmadhav <jithunmadhav1@gmail.com>
2023-06-08 23:28:10 +0530

    add vehicle updated

--- client/src/components/AddVehicle/AddVehicle.jsx
+++ client/src/components/AddVehicle/AddVehicle.jsx
@@ -11,8 +11,11 @@ function AddVehicle() {
     <div className='vehicle-background'>
       <div className='vehicle-inner-div'>
 
-        <h4 className='heading'>My Vehicle</h4>
+        <h4 className='vehicle-heading'>My Vehicle</h4>
         <button onClick={()=>openForm()} className='vehicle-btn' >+</button>
+        
+        <div className='vehicle-table'></div>
+
       </div>
     </div>
     
