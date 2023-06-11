5e32bbd2cf91233c3c9193bb137815aef197e115
jithunmadhav <jithunmadhav1@gmail.com>
2023-06-11 22:07:40 +0530

    appointment confirmation updated

---
+++ client/src/components/AppoinmentSuccess/AppoinmentSuccess.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './AppoinmentSuccess.css'
+function AppoinmentSuccess() {
+  return (
+    <>
+      <div>
+        <h4 className='success-header'>BOOKING SUCCESS</h4>
+      </div>
+    </>
+  )
+}
+
+export default AppoinmentSuccess
