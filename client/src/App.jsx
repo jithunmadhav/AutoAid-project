7ebe11e7699d91f205f5234c1cd7c721a698dc64
jithunmadhav <jithunmadhav1@gmail.com>
2023-06-12 11:12:13 +0530

    bugs fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -91,6 +91,7 @@ function App() {
 
           {user.login && (
             <>
+             <Route element={<Navigate to={'/'} />} path={'/user/login'} />
               <Route element={<HomePage />} path="/" />
               <Route element={<ProfilePage />} path="/user/profile" />
               <Route element={<PlacePage/>} path={'/location'} />
