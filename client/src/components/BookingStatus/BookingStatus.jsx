efdc8731e40e6703918ac5187b586b4617f0a8a2
jithunmadhav <jithunmadhav1@gmail.com>
2023-06-25 22:36:43 +0530

    booking status updated

--- client/src/components/BookingStatus/BookingStatus.jsx
+++ client/src/components/BookingStatus/BookingStatus.jsx
@@ -1,100 +1,143 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import UserBookingHistory from '../UserBookingHistory/UserBookingHistory'
 import './BookingStatus.css'
 import {
-    MDBCard,
-    MDBCardBody,
-    MDBCol,
-    MDBContainer,
-    MDBProgress,
-    MDBProgressBar,
-    MDBRow,
-  } from "mdb-react-ui-kit";
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBRow,
+  MDBTypography,
+} from "mdb-react-ui-kit";
 function BookingStatus(props) {
- console.log(props.data);
-    const [status, setStatus] = useState("Confirmed");
-  
-    const handleStatusChange = (newStatus) => {
-      setStatus(newStatus);
-    };
-  
-    const calculateProgressWidth = () => {
-      if (status === "Waiting for Confirmation") {
-        return 0;
-      } else if (status === "Confirmed") {
-        return 33;
-      } else if (status === "Progress") {
-        return 66;
-      } else if (status === "Completed") {
-        return 100;
-      }
-    };
-  return (
-    <div className='bookingstatus-bg'>
-          <MDBContainer className="py-5">
-      <MDBRow className="justify-content-center">
-        <MDBCol lg="10">
-           
-           
-              <MDBRow style={{ marginTop:'290px' }} className="align-items-center">
-                 
-                <MDBCol md="10">
-                  <MDBProgress style={{ height: "6px", borderRadius: "16px" }}>
-                    <MDBProgressBar
-                      style={{ borderRadius: "16px", backgroundColor: "blue" }}
-                      width={calculateProgressWidth()}
-                      valuemin={0}
-                      valuemax={100}
-                    />
-                  </MDBProgress>
-                  <div className="d-flex justify-content-around mb-1">
-                  <p className="text-muted mt-1 mb-0 small">pending</p>
-                  <p className="text-muted mt-1 mb-0 small">confirm </p>
-                    <p className="text-muted mt-1 mb-0 small">Work Progress</p>
-                    <p className="text-muted mt-1 mb-0 small">Completed</p>
-                  </div>
-                </MDBCol>
-              </MDBRow>
-         {/* <br/>
-        
-          <div className="d-flex justify-content-around">
-              <button
-                className={`status-button ${
-                  status === "Waiting for Confirmation" ? "active" : ""
-                }`}
-                onClick={() => handleStatusChange("Waiting for Confirmation")}
-              >
-                Waiting for Confirmation
-              </button>
-              <button
-                className={`status-button ${
-                  status === "Confirmed" ? "active" : ""
-                }`}
-                onClick={() => handleStatusChange("Confirmed")}
-              >
-                Confirmed
-              </button>
-              <button
-                className={`status-button ${
-                  status === "Progress" ? "active" : ""
-                }`}
-                onClick={() => handleStatusChange("Progress")}
-              >
-                Progress
-              </button>
-              <button
-                className={`status-button ${
-                  status === "Completed" ? "active" : ""
-                }`}
-                onClick={() => handleStatusChange("Completed")}
-              >
-                Completed
-              </button>
-            </div> */}
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-    </div>
-  )
+  const [status, setStatus] = useState(null); // Initial status is 0, indicating the first step
+const [closeStatus, setcloseStatus] = useState(false)
+  useEffect(() => {
+  if (props.data.status === 'waiting for comfirmation') {
+    setStatus(0);
+  } else if (props.data.status === 'confirmed') {
+    setStatus(1);
+  } else if (props.data.status === 'work progressing') {
+    setStatus(2);
+  } else if (props.data.status === 'completed') {
+    setStatus(3);
+  }
+}, []); // Empty dependency array to run the effect only once
+
+
+ return (
+  closeStatus ? <UserBookingHistory/> :
+   <>
+     <section className="vh-100 bookingstatus-bg" >
+     <button onClick={()=>setcloseStatus(true)}  className='bookingstatus-btn'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+            </svg>
+          </button>
+       <MDBContainer className="py-5 h-100">
+         <MDBRow className="justify-content-center align-items-center h-100">
+           <MDBCol size="12">
+             <MDBCard
+               className="card-stepper text-black"
+               style={{ borderRadius: "16px" }}
+             >
+               <MDBCardBody className="p-5">
+                 <div className="d-flex justify-content-between align-items-center mb-5">
+                   <div>
+                     <MDBTypography tag="h5" className="mb-0">
+                       Technician Info :{" "}
+                       <span className="text-primary font-weight-bold">
+                         {props.data.mechanic_name}
+                       </span>
+                     </MDBTypography>
+                   </div>
+                   <div className="text-end">
+                     <p className="mb-0">
+                    Technician Mobile :<span>{props.data.mechanic_mobile}</span>                     </p>
+                     <p className="mb-0">
+                       call and confirm for speedy service
+                     </p>
+                   </div>
+                 </div>
+                 <ul
+                   id="progressbar-2"
+                   className="d-flex justify-content-between mx-0 mt-0 mb-5 px-0 pt-0 pb-2"
+                 >
+                   <li
+                     className={`step0 ${
+                       status >= 0 ? "active" : ""
+                     } text-center`}
+                     id="step1"
+                   ></li>
+                   <li
+                     className={`step0 ${
+                       status >= 1 ? "active" : ""
+                     } text-center`}
+                     id="step2"
+                   ></li>
+                   <li
+                     className={`step0 ${
+                       status >= 2 ? "active" : ""
+                     } text-center`}
+                     id="step3"
+                   ></li>
+                   <li
+                     className={`step0 ${
+                       status >= 3 ? "active" : ""
+                     } text-center`}
+                     id="step4"
+                   ></li>
+                 </ul>
+
+                 <div className="d-flex justify-content-between">
+                   <div className="d-lg-flex align-items-center">
+                     <MDBIcon
+                       fas
+                       icon="clipboard-list me-lg-4 mb-3 mb-lg-0"
+                       size="3x"
+                     />
+                     <div>
+                       <p className="fw-bold mb-1">Waiting for</p>
+                       <p className="fw-bold mb-0">confirmation</p>
+                     </div>
+                   </div>
+                   <div className="d-lg-flex align-items-center">
+                     <MDBIcon
+                       fas
+                       icon="box-open me-lg-4 mb-3 mb-lg-0"
+                       size="3x"
+                     />
+                     <div>
+                       <p className="fw-bold mb-1">confirmed</p>
+                     </div>
+                   </div>
+                   <div className="d-lg-flex align-items-center">
+                     <MDBIcon
+                       fas
+                       icon="shipping-fast me-lg-4 mb-3 mb-lg-0"
+                       size="3x"
+                     />
+                     <div>
+                       <p className="fw-bold mb-1">work in</p>
+                       <p className="fw-bold mb-0">progress</p>
+                     </div>
+                   </div>
+                   <div className="d-lg-flex align-items-center">
+                     
+                     <div>
+                       <p className="fw-bold mb-1">Completed</p>
+                     </div>
+                   </div>
+                 </div>
+               </MDBCardBody>
+             </MDBCard>
+           </MDBCol>
+         </MDBRow>
+       </MDBContainer>
+     </section>
+   </>
+ );
 }
 
 export default BookingStatus
--- client/src/components/Sample/StripeSample.jsx
+++ client/src/components/Sample/StripeSample.jsx
@@ -45,9 +45,8 @@
 // };
 
 // export default RefundForm;
-
-import React from "react";
-import './sample.css'
+import React, { useState } from "react";
+import "./sample.css";
 import {
   MDBCard,
   MDBCardBody,
@@ -59,84 +58,74 @@ import {
 } from "mdb-react-ui-kit";
 
 export default function OrderDetails6() {
-  return (
-    <>
-      <section className="vh-100" style={{ backgroundColor: "#8c9eff" }}>
-        <MDBContainer className="py-5 h-100">
-          <MDBRow className="justify-content-center align-items-center h-100">
-            <MDBCol size="12">
-              <MDBCard
-                className="card-stepper text-black"
-                style={{ borderRadius: "16px" }}
-              >
-                <MDBCardBody className="p-5">
-                  <div className="d-flex justify-content-between align-items-center mb-5">
-                    <div>
-                      <MDBTypography tag="h5" className="mb-0">
-                        INVOICE{" "}
-                        <span className="text-primary font-weight-bold">
-                          #Y34XDHR
-                        </span>
-                      </MDBTypography>
-                    </div>
-                    <div className="text-end">
-                      <p className="mb-0">
-                        Expected Arrival <span>01/12/19</span>
-                      </p>
-                      <p className="mb-0">
-                        USPS{" "}
-                        <span className="font-weight-bold">
-                          234094567242423422898
-                        </span>
-                      </p>
-                    </div>
-                  </div>
-                  <ul
-                    id="progressbar-2"
-                    className="d-flex justify-content-between mx-0 mt-0 mb-5 px-0 pt-0 pb-2"
-                  >
-                    <li className="step0 active text-center" id="step1"></li>
-                    <li className="step0 active text-center" id="step2"></li>
-                    <li className="step0 active text-center" id="step3"></li>
-                    <li className="step0 active text-center" id="step4"></li>
-                  </ul>
+  const [status, setStatus] = useState(0); // Initial status is 0, indicating the first step
 
-                  <div className="d-flex justify-content-between">
-                    <div className="d-lg-flex align-items-center">
-                      <MDBIcon fas icon="clipboard-list me-lg-4 mb-3 mb-lg-0" size="3x" />
-                      <div>
-                        <p className="fw-bold mb-1">Order</p>
-                        <p className="fw-bold mb-0">Processed</p>
-                      </div>
-                    </div>
-                    <div className="d-lg-flex align-items-center">
-                      <MDBIcon fas icon="box-open me-lg-4 mb-3 mb-lg-0" size="3x" />
-                      <div>
-                        <p className="fw-bold mb-1">Order</p>
-                        <p className="fw-bold mb-0">Shipped</p>
-                      </div>
-                    </div>
-                    <div className="d-lg-flex align-items-center">
-                      <MDBIcon fas icon="shipping-fast me-lg-4 mb-3 mb-lg-0" size="3x" />
-                      <div>
-                        <p className="fw-bold mb-1">Order</p>
-                        <p className="fw-bold mb-0">En Route</p>
-                      </div>
-                    </div>
-                    <div className="d-lg-flex align-items-center">
-                      <MDBIcon fas icon="home me-lg-4 mb-3 mb-lg-0" size="3x" />
-                      <div>
-                        <p className="fw-bold mb-1">Order</p>
-                        <p className="fw-bold mb-0">Arrived</p>
-                      </div>
-                    </div>
+  const handleStatusChange = (newStatus) => {
+    setStatus(newStatus);
+  };
+
+  return (
+    <MDBContainer fluid className="vh-100 bg-primary">
+      <MDBRow className="vh-100 align-items-center">
+        <MDBCol md="8" className="mx-auto">
+          <MDBCard className="card-stepper text-black" style={{ borderRadius: "16px" }}>
+            <MDBCardBody className="p-5">
+              <div className="d-flex justify-content-between align-items-center mb-5">
+                <div>
+                  <MDBTypography tag="h5" className="mb-0">
+                    INVOICE <span className="text-primary font-weight-bold">#Y34XDHR</span>
+                  </MDBTypography>
+                </div>
+                <div className="text-end">
+                  <p className="mb-0">
+                    Expected Arrival <span>01/12/19</span>
+                  </p>
+                  <p className="mb-0">
+                    USPS <span className="font-weight-bold">234094567242423422898</span>
+                  </p>
+                </div>
+              </div>
+              <ul className="progressbar-2 d-flex justify-content-between mx-0 mt-0 mb-5 px-0 pt-0 pb-2">
+                <li className={`step0 ${status >= 0 ? "active" : ""} text-center`} id="step1"></li>
+                <li className={`step0 ${status >= 1 ? "active" : ""} text-center`} id="step2"></li>
+                <li className={`step0 ${status >= 2 ? "active" : ""} text-center`} id="step3"></li>
+                <li className={`step0 ${status >= 3 ? "active" : ""} text-center`} id="step4"></li>
+              </ul>
+
+              <div className="d-flex justify-content-between flex-wrap">
+                <div className="d-flex align-items-center mb-4 mb-md-0">
+                  <MDBIcon fas icon="clipboard-list me-4" size="3x" />
+                  <div>
+                    <p className="fw-bold mb-1">Order</p>
+                    <p className="fw-bold mb-0">Processed</p>
                   </div>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </section>
-    </>
+                </div>
+                <div className="d-flex align-items-center mb-4 mb-md-0">
+                  <MDBIcon fas icon="box-open me-4" size="3x" />
+                  <div>
+                    <p className="fw-bold mb-1">Order</p>
+                    <p className="fw-bold mb-0">Shipped</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center mb-4 mb-md-0">
+                  <MDBIcon fas icon="shipping-fast me-4" size="3x" />
+                  <div>
+                    <p className="fw-bold mb-1">Order</p>
+                    <p className="fw-bold mb-0">En Route</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center">
+                  <MDBIcon fas icon="home me-4" size="3x" />
+                  <div>
+                    <p className="fw-bold mb-1">Order</p>
+                    <p className="fw-bold mb-0">Arrived</p>
+                  </div>
+                </div>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
