09b1286b64485bae7b6ec158588b47c73e2367a5
jithunmadhav <jithunmadhav1@gmail.com>
2023-07-01 09:37:00 +0530

    UI updated

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
  
 
 const instance = axios.create({
-    // baseURL:'http://192.168.133.127:4000/',
-    baseURL:'http://localhost:4000',
+     baseURL:'http://192.168.0.73:4000/',
+    // baseURL:'http://localhost:4000',
      
   });
 
