12ca24f67bb0ba34d6b38447494edc8f041cd819
jithunmadhav <jithunmadhav1@gmail.com>
2023-07-10 20:32:54 +0530

    server updated

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"]
+    origin: ["http://localhost:3000",'http://autoaid.online/']
   },
 });
 
