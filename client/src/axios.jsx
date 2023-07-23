208ec27d5ea7b4d278dc0aeb602ab5cb5fc88040
jithunmadhav <jithunmadhav1@gmail.com>
2023-07-23 13:17:58 +0530

    server updated

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -2,7 +2,9 @@ import axios from "axios";
  
 
 const instance = axios.create({
-    baseURL:'https://server.autoaid.online/',
+  baseURL:' https://autoaid.onrender.com/',
+ 
+    // baseURL:'https://server.autoaid.online/',
     // baseURL:'http://localhost:4000/',
 
      
